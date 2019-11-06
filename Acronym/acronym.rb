bdd8eee02c143ce646a5cbd7e060450a3eca6944
gannagoodkevich <gannagoodkevich@gmail.com>
2019-11-06 14:20:50 +0300

    make test check on acronym

--- Acronym/acronym.rb
+++ Acronym/acronym.rb
@@ -1,13 +1,20 @@
+=begin
+Write your code for the 'Acronym' exercise in this file. Make the tests in
+`acronym_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/acronym` directory.
+=end
 class Acronym
-  def create_acronym(string)
+  def Acronym.abbreviate(string)
     words = string.split(" ")
-    print "The acronym is: "
-    words.each {|word| print word[0].upcase}
-    puts
+    acr = ""
+    words.each do |word|
+      acr << word[0].upcase if word[0].match(/[a-zA-Z]/)
+      if word.match(/-[a-z]/)
+        acr << word.split('-')[1][0].upcase
+      end
+    end
+    return acr
   end
 end
-
-puts "Enter the phrase you want to make acronym"
-phrase = gets.chomp
-acr = Acronym.new
-acr.create_acronym(phrase)
