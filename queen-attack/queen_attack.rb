191563bd95e8e314e8d669118f3ca1ee5a88530e
gannagoodkevich <gannagoodkevich@gmail.com>
2019-11-18 16:06:59 +0300

    refactor due to rubocop

--- queen-attack/queen_attack.rb
+++ queen-attack/queen_attack.rb
@@ -9,44 +9,76 @@ To get started with TDD, see the `README.md` file in your
 class Queens
   def initialize(hash)
     @hash = hash
-    @hash.each do |k, v|
-      raise ArgumentError, 'Argument is wrong' if v[0] < 0 || v[1] < 0 || v[0] >= 8 || v[1] >= 8
-    end
+    valid_hash_check
+    @flag = 0
   end
 
   def attack?
-    @board = Array.new(8){Array.new(8)}
-    @board.each do |line|
-      line.each {|element| element = 0}
-    end
-    flag = 0
-    @board[@hash[:white][0].to_i][@hash[:white][1].to_i] = 1
-    @board[@hash[:black][0].to_i][@hash[:black][1].to_i] = 1
-    flag = 1 if @hash[:white][0] == @hash[:black][0] || @hash[:white][1] == @hash[:black][1]
-    i = 0
-    while @hash[:white][0]-i > 0 && @hash[:white][1]+i < 7
-      i += 1
-      flag = 1 if (@hash[:white][0]-i == @hash[:black][0] and @hash[:white][1]+i == @hash[:black][1])
-    end
-    i = 0
-    while @hash[:white][0] + i < 7 && @hash[:white][1] + i < 7
-      i += 1
-      flag = 1 if (@hash[:white][0]+i == @hash[:black][0] and @hash[:white][1]+i == @hash[:black][1])
-    end
-    i = 0
-    while @hash[:white][0].to_i+i < 7 && @hash[:white][1].to_i-i > 0
-      i += 1
-      flag = 1 if (@hash[:white][0]+i == @hash[:black][0] and @hash[:white][1]-i == @hash[:black][1])
-    end
-    i = 0
-    while @hash[:white][0].to_i-i > 0 && @hash[:white][1].to_i-i > 0
-      i += 1
-      flag = 1 if (@hash[:white][0]-i == @hash[:black][0] and @hash[:white][1]-i == @hash[:black][1])
-    end
-    return true if flag == 1
+    attack_colomn(@hash[:white][1], @hash[:black][1])
+    attack_row(@hash[:white][0], @hash[:black][0])
+    attack_right_upper_diagonal(@hash[:white][0], @hash[:white][1], @hash[:black][0], @hash[:black][1])
+    attack_right_bottom_diagonal(@hash[:white][0], @hash[:white][1], @hash[:black][0], @hash[:black][1])
+    attack_left_bottom_diagonal(@hash[:white][0], @hash[:white][1], @hash[:black][0], @hash[:black][1])
+    attack_left_upper_diagonal(@hash[:white][0], @hash[:white][1], @hash[:black][0], @hash[:black][1])
+    return true if @flag == 1
   end
+
+  private
+
+  def valid_hash_check
+    @hash.each do |k, v|
+      if v[0] < 0 || v[1] < 0 || v[0] >= 8 || v[1] >= 8
+        raise ArgumentError, 'Argument is wrong'
+      end
+    end
+  end
+
+  def attack_right_upper_diagonal(row_position_white, colomn_position_white, row_position_black, colomn_position_black)
+    i = 0
+    while row_position_white - i > 0 && colomn_position_white + i < 7
+      i += 1
+      if row_position_white - i == row_position_black && colomn_position_white + i == colomn_position_black
+        @flag = 1
+      end
+    end
+  end
+
+  def attack_right_bottom_diagonal(row_position_white, colomn_position_white, row_position_black, colomn_position_black)
+    i = 0
+    while row_position_white + i < 7 && colomn_position_white + i < 7
+      i += 1
+      if row_position_white + i == row_position_black && colomn_position_white + i == colomn_position_black
+        @flag = 1
+      end
+    end
+  end
+
+  def attack_left_bottom_diagonal(row_position_white, colomn_position_white, row_position_black, colomn_position_black)
+    i = 0
+    while row_position_white + i < 7 && colomn_position_white - i > 0
+      i += 1
+      if row_position_white + i == row_position_black && colomn_position_white - i == colomn_position_black
+        @flag = 1
+      end
+    end
+  end
+
+  def attack_left_upper_diagonal(row_position_white, colomn_position_white, row_position_black, colomn_position_black)
+    i = 0
+    while row_position_white - i > 0 && colomn_position_white - i > 0
+      i += 1
+      if row_position_white - i == row_position_black && colomn_position_white - i == colomn_position_black
+        @flag = 1
+      end
+    end
+  end
+
+  def attack_row(row_position_white, row_position_black)
+    @flag = 1 if row_position_white == row_position_black
+  end
+
+  def attack_colomn(colomn_position_white, colomn_position_black)
+    @flag = 1 if colomn_position_white == colomn_position_black
+  end
+
 end
-
-
-q = Queens.new(white: [2, 2], black: [0, 4])
-puts q.attack?.inspect
