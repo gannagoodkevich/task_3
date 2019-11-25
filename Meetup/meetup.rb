6420038fb98943d7e1ff873e29bf0a0b42f551d9
gannagoodkevich <gannagoodkevich@gmail.com>
2019-11-25 17:28:17 +0300

    Add DAY_INDEXES to replace send method on cwday

--- Meetup/meetup.rb
+++ Meetup/meetup.rb
@@ -2,22 +2,25 @@ require 'date'
 
 class Meetup
   WEEK = 7
+  TWO_WEEKS = 14
+  THREE_WEEKS = 21
   LAST_MONTH = 12
-
+  DAY_INDEXES = {monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7}
   def initialize(month, year)
     @date = Date.new(year, month)
   end
 
   def day(day, schedule)
-    @date += 1 until @date.send("#{day}?") unless schedule == :last
+    day_index = DAY_INDEXES[day]
+    @date += 1 until @date.cwday == day_index unless schedule == :last
 
     case schedule
     when :second
       @date += WEEK
     when :third
-      @date += 2 * WEEK
+      @date += TWO_WEEKS
     when :fourth
-      @date += 3 * WEEK
+      @date += THREE_WEEKS
     when :teenth
       @date += WEEK until (13..19).include?(@date.day)
     when :last
