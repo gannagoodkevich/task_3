a88f6fbe5d5e02228c09ed3332145d5a548bf177
gannagoodkevich <gannagoodkevich@gmail.com>
2019-11-25 13:08:06 +0300

    rafactore to test scenarios

--- capybara/spec/capybara_spec.rb
+++ capybara/spec/capybara_spec.rb
@@ -1,43 +1,90 @@
-require 'capybara'
+require 'rspec'
+require 'capybara/rspec'
 require 'csv'
 
-class OnlinerParser
-  def parse_main_page_for_news(url = 'https://onliner.by')
-    @session = Capybara::Session.new(:selenium_chrome)
-    @session.visit(url)
-    parse_news_page('section.b-main-page-news-2__main-news article div h2 a', 'div.news-header__title h1', 'div.news-text p', 'div.news-header__image')
-    puts "__END__OF__SECTION__"
-    parse_news_page('section.b-main-page-news-2__secondary-news article h2 a', 'div.news-header__title h1', 'div.news-text p', 'div.news-header__image')
-    puts "__END__OF__SECTION__"
-    parse_news_page('li.b-teasers-2__teaser a', 'div.news-header__title h1', 'div.news-text p', 'div.news-header__image')
-    puts "__END__OF__SECTION__"
-    parse_news_page('ul.b-main-page-news-2__news-list li div h3 a', 'div.news-header__title h1', 'div.news-text p', 'div.news-header__image')
-    puts "__END__OF__PROCCESSING__"
-  end
+describe 'Parse Onliner.by' do
+  let(:tag_to_find_title) { 'div.news-header__title h1' }
+  let(:tag_to_find_text) { 'div.news-text p' }
+  let(:tag_to_find_image) { 'div.news-header__image' }
 
-  private
 
-  def parse_news_page(tag_to_find_news, tag_to_find_title, tag_to_find_text, tag_to_find_image)
-    @session.find_all(tag_to_find_news).uniq do |link|
-      puts link[:href]
+  scenario "Parse main news" do
+    session1 = Capybara::Session.new(:selenium_chrome)
+    session1.visit('https://onliner.by')
+    session1.find_all('section.b-main-page-news-2__main-news article div h2 a').uniq do |link|
       if link[:href].to_s.match(/(forum)|(kurs)/).nil?
-        parse_info(link, tag_to_find_title, tag_to_find_text, tag_to_find_image)
+        session1 = Capybara::Session.new(:selenium_chrome)
+        session1.visit(link[:href])
+        title = session1.find(tag_to_find_title).text
+        description = session1.find_all(tag_to_find_text)[0].text.chars.take(200).join
+        image = session1.find(tag_to_find_image)[:style].gsub("background-image: url(", "")
+        CSV.open("answer.csv", "a+") do |csv|
+          csv << [title, description, image]
+        end
+        expect(session1.current_url).to eql(link[:href])
+        session1.driver.quit
       end
     end
-  end
-
-  def parse_info(link, tag_to_find_title, tag_to_find_text, tag_to_find_image)
-    session1 = Capybara::Session.new(:selenium_chrome)
-    session1.visit(link[:href])
-    csv_output(session1.find(tag_to_find_title).text, session1.find_all(tag_to_find_text)[0].text.chars.take(200).join, session1.find(tag_to_find_image)[:style].gsub("background-image: url(", ""))
     session1.driver.quit
   end
 
-  def csv_output(title, description, image)
-    CSV.open("answer.csv", "a+") do |csv|
-      csv << [title, description, image]
+  scenario "Parse secondary news" do
+    session1 = Capybara::Session.new(:selenium_chrome)
+    session1.visit('https://onliner.by')
+    session1.find_all('section.b-main-page-news-2__secondary-news article h2 a').uniq do |link|
+      if link[:href].to_s.match(/(forum)|(kurs)/).nil?
+        session1 = Capybara::Session.new(:selenium_chrome)
+        session1.visit(link[:href])
+        title = session1.find(tag_to_find_title).text
+        description = session1.find_all(tag_to_find_text)[0].text.chars.take(200).join
+        image = session1.find(tag_to_find_image)[:style].gsub("background-image: url(", "")
+        CSV.open("answer.csv", "a+") do |csv|
+          csv << [title, description, image]
+        end
+        expect(session1.current_url).to eql(link[:href])
+        session1.driver.quit
+      end
     end
+    session1.driver.quit
+  end
+
+  scenario "Parse teaser news" do
+    session1 = Capybara::Session.new(:selenium_chrome)
+    session1.visit('https://onliner.by')
+    session1.find_all('li.b-teasers-2__teaser a').uniq do |link|
+      if link[:href].to_s.match(/(forum)|(kurs)/).nil?
+        session1 = Capybara::Session.new(:selenium_chrome)
+        session1.visit(link[:href])
+        title = session1.find(tag_to_find_title).text
+        description = session1.find_all(tag_to_find_text)[0].text.chars.take(200).join
+        image = session1.find(tag_to_find_image)[:style].gsub("background-image: url(", "")
+        CSV.open("answer.csv", "a+") do |csv|
+          csv << [title, description, image]
+        end
+        expect(session1.current_url).to eql(link[:href])
+        session1.driver.quit
+      end
+    end
+    session1.driver.quit
+  end
+
+  scenario "Parse list news" do
+    session1 = Capybara::Session.new(:selenium_chrome)
+    session1.visit('https://onliner.by')
+    session1.find_all('ul.b-main-page-news-2__news-list li div h3 a').uniq do |link|
+      if link[:href].to_s.match(/(forum)|(kurs)/).nil?
+        session1 = Capybara::Session.new(:selenium_chrome)
+        session1.visit(link[:href])
+        title = session1.find(tag_to_find_title).text
+        description = session1.find_all(tag_to_find_text)[0].text.chars.take(200).join
+        image = session1.find(tag_to_find_image)[:style].gsub("background-image: url(", "")
+        CSV.open("answer.csv", "a+") do |csv|
+          csv << [title, description, image]
+        end
+        expect(session1.current_url).to eql(link[:href])
+        session1.driver.quit
+      end
+    end
+    session1.driver.quit
   end
 end
-
-OnlinerParser.new.parse_main_page_for_news
