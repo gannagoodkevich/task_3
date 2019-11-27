048bb8c5da04a1e5283e29c82b97c71106dba68a
gannagoodkevich <gannagoodkevich@gmail.com>
2019-11-27 17:22:14 +0300

    Use Time instead of Range to make all logical

--- unit_test/lib/time_crosser.rb
+++ unit_test/lib/time_crosser.rb
@@ -8,30 +8,40 @@ class TimeCrosser
 
   def match_time_intervals
     flag = 0
-    intervals = []
-    @time_intervals.each do |interval|
-      intervals << convert_to_range(interval[0], interval[1])
+    intervals = @time_intervals.map do |interval|
+      convert_to_time(interval[0], interval[1])
     end
-    while flag.zero?
-      break if intervals.size == 1
-
-      @result = []
-      flag = 1
-      prev_interval = intervals.shift
-      intervals.each do |interval|
-        @result = @result.flatten
-        if compare(prev_interval, interval)
-          @result.delete(prev_interval) if flag == 1
-          flag = 0
-        end
-        prev_interval = interval
+    puts intervals.inspect
+    puts "INTERVALS"
+    puts intervals.inspect
+    puts "-^-"
+    @result = []
+    flag = 1
+    prev_interval = intervals.shift
+    intervals.each do |interval|
+      #@result = @result.flatten
+      interval = interval.flatten
+      puts "--PREV--"
+      puts prev_interval.inspect
+      puts "--INT--"
+      puts interval.inspect
+      puts "---"
+      if compare(prev_interval, interval) || compare(interval, prev_interval)
+        puts "HOLA"
+        puts @result.inspect
+        flag = 0
       end
-      intervals = @result.flatten.uniq
+      puts "INTERVALS"
+      puts intervals.inspect
+      puts "-^-"
     end
+    intervals = @result.uniq
+    puts "--RES--"
+    puts @result.uniq.flatten.inspect
     @result = @time_intervals if @result == []
     answer = []
-    @result.flatten.uniq.each do |interval|
-      answer << convert_result(interval.begin, interval.end)
+    @result.flatten.uniq.each_slice(2) do |time_f, time_s|
+      answer << %W[#{time_f} #{time_s}]
     end
     answer
   end
@@ -40,36 +50,32 @@ class TimeCrosser
 
   def compare(first, second)
     flag = true
-    if first.cover?(second.begin) && first.cover?(second.end)
-      result = first
-    elsif second.cover?(first.begin) && second.cover?(first.end)
-      result = second
-    elsif first.cover?(second.begin) && second.cover?(first.end)
-      result = first.begin..second.end
-    elsif second.cover?(first.begin) && first.cover?(second.end)
-      result = second.begin..first.end
-    else
+    result = first
+    puts first[0].inspect
+    puts second[0].inspect
+    puts "Star checking"
+    if first[0].between?(second[0], second[1])
+      result[0] = second[0]
+    end
+    if first[1].between?(second[0], second[1])
+      result[1] = second[1]
+    end
+    unless first[0].between?(second[0], second[1]) || first[1].between?(second[0], second[1])
       flag = false
       result = []
-      result << first
-      result << second
     end
+    #puts flag
     @result << result
     flag
   end
 
-  def convert_to_range(begin_interval, end_interval)
+  def convert_to_time(begin_interval, end_interval)
     begin_interval = begin_interval.split(':')
     end_interval = end_interval.split(':')
-    begin_numeric = begin_interval[0].to_i * 100 + begin_interval[1].to_i
-    end_numeric = end_interval[0].to_i * 100 + end_interval[1].to_i
-    begin_numeric..end_numeric
+    return Time.gm(1999, 12, 10, begin_interval[0], begin_interval[1]), Time.gm(1999, 12, 10, end_interval[0], end_interval[1])
   end
 
   def convert_result(time_first, time_second)
-    time_first = [time_first / 100, time_first % 100]
-    time_second = [time_second / 100, time_second % 100]
-    time_interval = %W[#{time_first[0]}:#{time_first[1]} #{time_second[0]}:#{time_second[1]}]
-    time_interval.map { |interval| interval.gsub(':0', ':00') }
+    %W[#{time_first.strftime('%H:%M')} #{time_second.strftime('%H:%M')}]
   end
 end
