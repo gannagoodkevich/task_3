02a5663b828938e4c8f95964366d0b7951b0c8aa
gannagoodkevich <gannagoodkevich@gmail.com>
2019-11-28 12:30:24 +0300

    Remove unnecessary init-s, replace glag on continue_sticking

--- unit_test/lib/time_crosser.rb
+++ unit_test/lib/time_crosser.rb
@@ -6,7 +6,6 @@ class TimeCrosser
   DAY = 1
   def initialize(time_intervals)
     @time_intervals = time_intervals
-    @result = []
   end
 
   def match_time_intervals
@@ -14,7 +13,6 @@ class TimeCrosser
       convert_to_time(interval[0], interval[1])
     end
     find_matching_time(intervals)
-    @result = @time_intervals if @result.empty?
     @result.flatten.uniq.each_slice(2).map do |time_f, time_s|
       %W[#{time_f.strftime('%H:%M')} #{time_s.strftime('%H:%M')}]
     end
@@ -29,17 +27,17 @@ class TimeCrosser
   end
 
   def find_matching_time(intervals)
-    flag = 0
-    while flag.zero?
+    continue_sticking = true
+    while continue_sticking
       break if intervals.size == 1
 
       @result = []
-      flag = 1
+      continue_sticking = false
       intervals.each_cons(2) do |prev_interval, interval|
         if prev_interval[0] <= interval[1] && prev_interval[1] >= interval[0]
           @result << prev_interval.concat(interval).minmax
-          @result.delete(prev_interval) if flag == 1
-          flag = 0
+          @result.delete(prev_interval) if continue_sticking == false
+          continue_sticking = true
         else
           @result << prev_interval << interval
         end
